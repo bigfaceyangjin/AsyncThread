6e30b93800176be8a3c63fdf8231b5d1c475b28f
yangjin <2822102009@qq.com>
2022-05-21 00:01:26 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyAsyncThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
