14cca065c7e7d28167f220f7ca23bd3a57f8dccb
yangjin <2822102009@qq.com>
2022-06-06 22:36:52 +0800

    异步多线程第一节课完成

--- Zhan.AsyncThread.Project/Zhan.AsyncThread.Project/Form1.Designer.cs
+++ Zhan.AsyncThread.Project/Zhan.AsyncThread.Project/Form1.Designer.cs
@@ -30,13 +30,14 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.btnAdvance = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(41, 69);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 36);
+            this.button1.Size = new System.Drawing.Size(138, 36);
             this.button1.TabIndex = 0;
             this.button1.Text = "同步方法";
             this.button1.UseVisualStyleBackColor = true;
@@ -46,17 +47,28 @@
             // 
             this.button2.Location = new System.Drawing.Point(41, 168);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(94, 37);
+            this.button2.Size = new System.Drawing.Size(138, 37);
             this.button2.TabIndex = 1;
             this.button2.Text = "异步方法";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnAdvance
+            // 
+            this.btnAdvance.Location = new System.Drawing.Point(41, 263);
+            this.btnAdvance.Name = "btnAdvance";
+            this.btnAdvance.Size = new System.Drawing.Size(138, 35);
+            this.btnAdvance.TabIndex = 2;
+            this.btnAdvance.Text = "AsyncAdvance";
+            this.btnAdvance.UseVisualStyleBackColor = true;
+            this.btnAdvance.Click += new System.EventHandler(this.btnAdvance_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(817, 471);
+            this.Controls.Add(this.btnAdvance);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -70,6 +82,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnAdvance;
     }
 }
 
--- Zhan.AsyncThread.Project/Zhan.AsyncThread.Project/Form1.cs
+++ Zhan.AsyncThread.Project/Zhan.AsyncThread.Project/Form1.cs
@@ -33,12 +33,13 @@ namespace Zhan.AsyncThread.Project
 
         public void DoNothing(string name)
         {
+            Console.WriteLine($"*********************** DoNothing Start {name} " + Thread.CurrentThread.ManagedThreadId.ToString("x2") +" *********************************");
             long rs = 0;
             for (int i = 0; i < 1000000000; i++)
             {
                 rs += i;
-            }
-            Console.WriteLine(name+":"+Thread.CurrentThread.ManagedThreadId);
+            } 
+            Console.WriteLine($"*********************** DoNothing End {name} " + Thread.CurrentThread.ManagedThreadId.ToString("x2") +" *********************************");
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -50,6 +51,60 @@ namespace Zhan.AsyncThread.Project
                 action.BeginInvoke("btn_2",null,null);
             }
             Console.WriteLine("-----   end  -----");
+        }
+
+        private void btnAdvance_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("*********************** btnAdvance_Click_Start "+Thread.CurrentThread.ManagedThreadId.ToString("x2") +" *********************************");
+            Action<string> act1 = this.DoNothing;
+            IAsyncResult result = null;
+            //AsyncCallback callback = ar => {
+            //    Console.WriteLine(object.ReferenceEquals(result,ar)+result.AsyncState.ToString());
+            //    Console.WriteLine($"计算结束 {Thread.CurrentThread.ManagedThreadId.ToString("x2")}"); 
+            //};
+            result= act1.BeginInvoke("btnAdvance_Click", null, null);
+            //怎样保证最后结果代码在子线程计算之后
+            //方法一：利用iscompled属性判断异步是否完成 从而延迟主线程执行代码的时间
+            { 
+                //int i = 0;
+                //while(!result.IsCompleted)
+                //{    
+                //    if(i<10)
+                //    {
+                //        Console.WriteLine($"文件已下载{i++ * 10}%");
+                //    }
+                //    else
+                //    {
+                //        Console.WriteLine("文件已下载99.99%...");
+                //    } 
+                //    Thread.Sleep(100);
+                //}
+                //    Console.WriteLine("文件已下载完成。");
+            }
+            //方法二 根据信号量 即时等待 限时等待
+            Thread.Sleep(200);
+            Console.WriteLine("Do Something else......");
+            Console.WriteLine("Do Something else......");
+            Console.WriteLine("Do Something else......");
+            //result.AsyncWaitHandle.WaitOne();   //组织主线程，等待子线程任务完成
+            //result.AsyncWaitHandle.WaitOne(-1); //同上 
+            //result.AsyncWaitHandle.WaitOne(1000);//不管任务有没有完成 等待1000ms后主线程开始工作 限时等待
+
+
+            //方法三 endinvoke(); 不仅可以做到等待 还可以获取返回值 注意每一个异步操作只能使用一次endinvoke() 要么在回调里使用，要么在外面使用 否则异常
+            {
+                Func<string, int> func = n => { Console.WriteLine(n); Thread.Sleep(1000); return DateTime.Now.Day; };
+                Console.WriteLine("func.Invoke(zz):"+func.Invoke("zz"));//同步调用
+
+                //异步调用
+                IAsyncResult asyncResult = func.BeginInvoke("yj", c => {
+                    //一般情况下这个返回值时再回调中要用到
+                    int r= func.EndInvoke(c);
+                    Console.WriteLine(c.AsyncState); }, "牛皮");
+                //Console.WriteLine("func.EndInvoke(asyncResult):"+ func.EndInvoke(asyncResult));
+            }
+            Console.WriteLine("计算结果真的完成了");
+            Console.WriteLine($"*********************** btnAdvance_Click_End {Thread.CurrentThread.ManagedThreadId.ToString("x2")} *********************************");
         }
     }
 }
