06378c4507b68c77bf6797759107bf25b6e2af41
yangjin <2822102009@qq.com>
2022-06-12 16:40:05 +0800

    reset

--- Zhan.AsyncThread.Project/Zhan.AsyncThread.Project/Form1.Designer.cs
+++ Zhan.AsyncThread.Project/Zhan.AsyncThread.Project/Form1.Designer.cs
@@ -28,109 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.btn_tongbu = new System.Windows.Forms.Button();
-			this.btn_Async = new System.Windows.Forms.Button();
-			this.btnAdvance = new System.Windows.Forms.Button();
-			this.btnThreads = new System.Windows.Forms.Button();
-			this.btnThreadPool = new System.Windows.Forms.Button();
-			this.btn_Task = new System.Windows.Forms.Button();
-			this.btnParallel = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// btn_tongbu
-			// 
-			this.btn_tongbu.Location = new System.Drawing.Point(27, 46);
-			this.btn_tongbu.Margin = new System.Windows.Forms.Padding(2);
-			this.btn_tongbu.Name = "btn_tongbu";
-			this.btn_tongbu.Size = new System.Drawing.Size(92, 32);
-			this.btn_tongbu.TabIndex = 0;
-			this.btn_tongbu.Text = "同步方法";
-			this.btn_tongbu.UseVisualStyleBackColor = true;
-			this.btn_tongbu.Click += new System.EventHandler(this.button1_Click);
-			// 
-			// btn_Async
-			// 
-			this.btn_Async.Location = new System.Drawing.Point(27, 112);
-			this.btn_Async.Margin = new System.Windows.Forms.Padding(2);
-			this.btn_Async.Name = "btn_Async";
-			this.btn_Async.Size = new System.Drawing.Size(92, 38);
-			this.btn_Async.TabIndex = 1;
-			this.btn_Async.Text = "异步方法";
-			this.btn_Async.UseVisualStyleBackColor = true;
-			this.btn_Async.Click += new System.EventHandler(this.button2_Click);
-			// 
-			// btnAdvance
-			// 
-			this.btnAdvance.Location = new System.Drawing.Point(27, 175);
-			this.btnAdvance.Margin = new System.Windows.Forms.Padding(2);
-			this.btnAdvance.Name = "btnAdvance";
-			this.btnAdvance.Size = new System.Drawing.Size(92, 35);
-			this.btnAdvance.TabIndex = 2;
-			this.btnAdvance.Text = "AsyncAdvance";
-			this.btnAdvance.UseVisualStyleBackColor = true;
-			this.btnAdvance.Click += new System.EventHandler(this.btnAdvance_Click);
-			// 
-			// btnThreads
-			// 
-			this.btnThreads.Location = new System.Drawing.Point(166, 46);
-			this.btnThreads.Margin = new System.Windows.Forms.Padding(2);
-			this.btnThreads.Name = "btnThreads";
-			this.btnThreads.Size = new System.Drawing.Size(92, 32);
-			this.btnThreads.TabIndex = 3;
-			this.btnThreads.Text = "Threads";
-			this.btnThreads.UseVisualStyleBackColor = true;
-			this.btnThreads.Click += new System.EventHandler(this.btnThreads_Click);
-			// 
-			// btnThreadPool
-			// 
-			this.btnThreadPool.Location = new System.Drawing.Point(166, 112);
-			this.btnThreadPool.Margin = new System.Windows.Forms.Padding(2);
-			this.btnThreadPool.Name = "btnThreadPool";
-			this.btnThreadPool.Size = new System.Drawing.Size(92, 32);
-			this.btnThreadPool.TabIndex = 4;
-			this.btnThreadPool.Text = "ThreadPool";
-			this.btnThreadPool.UseVisualStyleBackColor = true;
-			this.btnThreadPool.Click += new System.EventHandler(this.btnThreadPool_Click);
-			// 
-			// btn_Task
-			// 
-			this.btn_Task.Location = new System.Drawing.Point(166, 175);
-			this.btn_Task.Margin = new System.Windows.Forms.Padding(2);
-			this.btn_Task.Name = "btn_Task";
-			this.btn_Task.Size = new System.Drawing.Size(92, 32);
-			this.btn_Task.TabIndex = 5;
-			this.btn_Task.Text = "Task";
-			this.btn_Task.UseVisualStyleBackColor = true;
-			this.btn_Task.Click += new System.EventHandler(this.btn_Task_Click);
-			// 
-			// btnParallel
-			// 
-			this.btnParallel.Location = new System.Drawing.Point(166, 240);
-			this.btnParallel.Margin = new System.Windows.Forms.Padding(2);
-			this.btnParallel.Name = "btnParallel";
-			this.btnParallel.Size = new System.Drawing.Size(92, 32);
-			this.btnParallel.TabIndex = 6;
-			this.btnParallel.Text = "Parallel";
-			this.btnParallel.UseVisualStyleBackColor = true;
-			this.btnParallel.Click += new System.EventHandler(this.btnParallel_Click);
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(545, 314);
-			this.Controls.Add(this.btnParallel);
-			this.Controls.Add(this.btn_Task);
-			this.Controls.Add(this.btnThreadPool);
-			this.Controls.Add(this.btnThreads);
-			this.Controls.Add(this.btnAdvance);
-			this.Controls.Add(this.btn_Async);
-			this.Controls.Add(this.btn_tongbu);
-			this.Margin = new System.Windows.Forms.Padding(2);
-			this.Name = "Form1";
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "Form1";
-			this.ResumeLayout(false);
+            this.btn_tongbu = new System.Windows.Forms.Button();
+            this.btn_Async = new System.Windows.Forms.Button();
+            this.btnAdvance = new System.Windows.Forms.Button();
+            this.btnThreads = new System.Windows.Forms.Button();
+            this.btnThreadPool = new System.Windows.Forms.Button();
+            this.btn_Task = new System.Windows.Forms.Button();
+            this.btnParallel = new System.Windows.Forms.Button();
+            this.btnThreadCore = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_tongbu
+            // 
+            this.btn_tongbu.Location = new System.Drawing.Point(40, 69);
+            this.btn_tongbu.Name = "btn_tongbu";
+            this.btn_tongbu.Size = new System.Drawing.Size(138, 48);
+            this.btn_tongbu.TabIndex = 0;
+            this.btn_tongbu.Text = "同步方法";
+            this.btn_tongbu.UseVisualStyleBackColor = true;
+            this.btn_tongbu.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_Async
+            // 
+            this.btn_Async.Location = new System.Drawing.Point(40, 168);
+            this.btn_Async.Name = "btn_Async";
+            this.btn_Async.Size = new System.Drawing.Size(138, 57);
+            this.btn_Async.TabIndex = 1;
+            this.btn_Async.Text = "异步方法";
+            this.btn_Async.UseVisualStyleBackColor = true;
+            this.btn_Async.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnAdvance
+            // 
+            this.btnAdvance.Location = new System.Drawing.Point(40, 262);
+            this.btnAdvance.Name = "btnAdvance";
+            this.btnAdvance.Size = new System.Drawing.Size(138, 52);
+            this.btnAdvance.TabIndex = 2;
+            this.btnAdvance.Text = "AsyncAdvance";
+            this.btnAdvance.UseVisualStyleBackColor = true;
+            this.btnAdvance.Click += new System.EventHandler(this.btnAdvance_Click);
+            // 
+            // btnThreads
+            // 
+            this.btnThreads.Location = new System.Drawing.Point(249, 69);
+            this.btnThreads.Name = "btnThreads";
+            this.btnThreads.Size = new System.Drawing.Size(138, 48);
+            this.btnThreads.TabIndex = 3;
+            this.btnThreads.Text = "Threads";
+            this.btnThreads.UseVisualStyleBackColor = true;
+            this.btnThreads.Click += new System.EventHandler(this.btnThreads_Click);
+            // 
+            // btnThreadPool
+            // 
+            this.btnThreadPool.Location = new System.Drawing.Point(249, 168);
+            this.btnThreadPool.Name = "btnThreadPool";
+            this.btnThreadPool.Size = new System.Drawing.Size(138, 48);
+            this.btnThreadPool.TabIndex = 4;
+            this.btnThreadPool.Text = "ThreadPool";
+            this.btnThreadPool.UseVisualStyleBackColor = true;
+            this.btnThreadPool.Click += new System.EventHandler(this.btnThreadPool_Click);
+            // 
+            // btn_Task
+            // 
+            this.btn_Task.Location = new System.Drawing.Point(249, 262);
+            this.btn_Task.Name = "btn_Task";
+            this.btn_Task.Size = new System.Drawing.Size(138, 48);
+            this.btn_Task.TabIndex = 5;
+            this.btn_Task.Text = "Task";
+            this.btn_Task.UseVisualStyleBackColor = true;
+            this.btn_Task.Click += new System.EventHandler(this.btn_Task_Click);
+            // 
+            // btnParallel
+            // 
+            this.btnParallel.Location = new System.Drawing.Point(249, 360);
+            this.btnParallel.Name = "btnParallel";
+            this.btnParallel.Size = new System.Drawing.Size(138, 48);
+            this.btnParallel.TabIndex = 6;
+            this.btnParallel.Text = "Parallel";
+            this.btnParallel.UseVisualStyleBackColor = true;
+            this.btnParallel.Click += new System.EventHandler(this.btnParallel_Click);
+            // 
+            // btnThreadCore
+            // 
+            this.btnThreadCore.Location = new System.Drawing.Point(40, 360);
+            this.btnThreadCore.Margin = new System.Windows.Forms.Padding(4);
+            this.btnThreadCore.Name = "btnThreadCore";
+            this.btnThreadCore.Size = new System.Drawing.Size(138, 48);
+            this.btnThreadCore.TabIndex = 8;
+            this.btnThreadCore.Text = "ThreadCore";
+            this.btnThreadCore.UseVisualStyleBackColor = true;
+            this.btnThreadCore.Click += new System.EventHandler(this.btnThreadCore_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 471);
+            this.Controls.Add(this.btnThreadCore);
+            this.Controls.Add(this.btnParallel);
+            this.Controls.Add(this.btn_Task);
+            this.Controls.Add(this.btnThreadPool);
+            this.Controls.Add(this.btnThreads);
+            this.Controls.Add(this.btnAdvance);
+            this.Controls.Add(this.btn_Async);
+            this.Controls.Add(this.btn_tongbu);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
 
         }
 
@@ -143,6 +148,7 @@
 		private System.Windows.Forms.Button btnThreadPool;
 		private System.Windows.Forms.Button btn_Task;
 		private System.Windows.Forms.Button btnParallel;
-	}
+        private System.Windows.Forms.Button btnThreadCore;
+    }
 }
 
--- Zhan.AsyncThread.Project/Zhan.AsyncThread.Project/Form1.cs
+++ Zhan.AsyncThread.Project/Zhan.AsyncThread.Project/Form1.cs
@@ -365,12 +365,132 @@ namespace Zhan.AsyncThread.Project
 			}
 			Console.WriteLine("*********** btnParallel_Click_End " + Thread.CurrentThread.ManagedThreadId.ToString("x2") + " ***********");
 		}
-		#endregion
-		/// <summary>
-		/// 线程测试的方法
-		/// </summary>
-		/// <param name="name"></param>
-		private void DoNothing(string name)
+        #endregion
+
+        #region 1.异常处理、线程取消、多线程的临时变量 2.线程安全和lock锁 3.await/async
+        private static readonly object btnThreadCoreLock = new object();//Lock锁引用类型 CSDN标准写法
+        private void btnThreadCore_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("*********** btnThreadCore_Click_Start " + Thread.CurrentThread.ManagedThreadId.ToString("x2") + " ***********");
+            try
+            {
+                #region 多线程异常处理
+                //TaskFactory taskFactory = new TaskFactory();
+                //List<Task> taskList = new List<Task>();
+                //for (int i = 0; i < 15; i++)
+                //{
+                //	string name = "btnThreadCore_Click_" + (i + 1);
+                //	Action<object> act = j =>
+                //	{
+                //		try
+                //		{
+                //			if (j.Equals($"btnThreadCore_Click_9") || j.Equals($"btnThreadCore_Click_10"))
+                //			{
+                //				throw new Exception($"{j} 执行失败");
+                //			}
+                //			Console.WriteLine($"{j} 执行成功");
+                //		}
+                //		catch (Exception ex)
+                //		{
+                //			Console.WriteLine(ex.Message);
+                //		}
+                //	};
+                //	taskList.Add(taskFactory.StartNew(act, name));
+                //}
+                #endregion
+
+                #region 线程的取消
+                //CancellationTokenSource cts = new CancellationTokenSource();
+                //List<Task> taskList = new List<Task>();
+                //TaskFactory taskFactory = new TaskFactory();
+                //for (int i = 0; i < 10; i++)
+                //{
+                //	string name = string.Format("btnThreadCore_{0}",i+1);
+                //	Action<object> act = x => {
+                //		try
+                //		{ 
+                //			if (x.ToString().Equals("btnThreadCore_5") || x.ToString().Equals("btnThreadCore_6"))
+                //			{
+                //				throw new Exception($"{x}执行失败 {Thread.CurrentThread.ManagedThreadId.ToString("x2")}");
+                //			}
+                //			if (cts.IsCancellationRequested)
+                //			{
+                //				Console.WriteLine($"{x}放弃执行 {Thread.CurrentThread.ManagedThreadId.ToString("x2")}");
+                //				return;
+                //			}
+                //			else {
+                //				Console.WriteLine($"{x}执行成功 {Thread.CurrentThread.ManagedThreadId.ToString("x2")}");
+                //			}
+                //		}
+                //		catch (Exception ex)
+                //		{
+                //			cts.Cancel();
+                //			Console.WriteLine(ex.Message);
+                //		}
+                //	};
+                //	taskList.Add(taskFactory.StartNew(act,name,cts.Token));
+                //}
+                //Task.WaitAll(taskList.ToArray());
+                #endregion
+
+                #region 临时变量 
+                //for (int i = 0; i < 10; i++)
+                //{
+                //	//Thread.Sleep(200);
+                //	int k = i;
+                //	k = i;
+                //	Task.Run(()=> {
+                //		Console.WriteLine($"k={k} i={i}");
+                //	});
+                //}
+                #endregion
+
+                #region 线程安全
+                List<int> numList = new List<int>();
+                List<Task> taskList = new List<Task>();
+                int total = 0;
+                for (int i = 0; i < 1000000; i++)
+                {
+                    int k = i;
+                    taskList.Add(Task.Run(() => {
+                        //lock后的方法块 只允许一个线程进入，相当于对这个引用变量占用这个引用链接 不要用string 因为享元
+                        //引用链接内存边界存在一个值为-1，当有人占用时为0，无人占用时为-1 虽然没了并发 但影响了性能
+                        lock (btnThreadCoreLock)
+                        {
+                            total += 1;
+                            numList.Add(k);
+                        }
+                        //Monitor与lock功能一致 {}就是锁住的内容 只允许单线程操作
+                        //Monitor.Enter(btnThreadCoreLock);
+                        //{
+
+                        //}
+                        //Monitor.Exit(btnThreadCoreLock);
+                    }));
+                }
+                Task.WaitAll(taskList.ToArray());
+                Console.WriteLine($"numList总数量：{numList.Count} total={total}");
+                #endregion
+            }
+            catch (AggregateException agp)
+            {
+                foreach (var item in agp.InnerExceptions)
+                {
+                    Console.WriteLine(item.Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("*********** btnThreadCore_Click_End " + Thread.CurrentThread.ManagedThreadId.ToString("x2") + " ***********");
+        }
+        #endregion
+        /// <summary>
+        /// 线程测试的方法
+        /// </summary>
+        /// <param name="name"></param>
+        private void DoNothing(string name)
 		{
 			Console.WriteLine($"************* DoNothing Start {name} " + Thread.CurrentThread.ManagedThreadId.ToString("x2") + " *************");
 			long rs = 0;
@@ -469,5 +589,8 @@ namespace Zhan.AsyncThread.Project
 			//manualReset.WaitOne();
 			return result;
 		}
-	}
+
+        
+         
+    }
 }
